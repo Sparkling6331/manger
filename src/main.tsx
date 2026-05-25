7379a9661d9c3258de30d3633c02836adfff6b60
Guillaume <79760746+Sparkling6331@users.noreply.github.com>
2026-05-25 22:00:05 +0200

    Use HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { seedIfEmpty } from './seed.ts'
@@ -9,8 +9,8 @@ seedIfEmpty().catch(console.error)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
